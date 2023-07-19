b2d695cd3fe4e2330315d19a1fcb8b31f9c9822d
john makateto <jmakateto@gmail.com>
2023-07-20 01:20:07 +0300

    completed assignement

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
-import React from "react";
-import MovieCard from "./MovieCard";
+
+import React from 'react';
+import MovieCard from './MovieCard'; 
 
 function App() {
   const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+  const posterURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      {/* Passing down props to MovieCard */}
+      <MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
     </div>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,13 @@
-import React from "react";
 
-function MovieCard({
-  title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
-}) {
+import React from 'react';
+
+function MovieCard({ title, posterSrc, genres }) {
+  const defaultPosterURL = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg";
+  const posterURL = posterSrc || defaultPosterURL;
+
   return (
     <div className="movie-card">
-      <img src={posterSrc} alt={title} />
+      <img src={posterURL} alt={title} />
       <h2>{title}</h2>
       <small>{genres.join(", ")}</small>
     </div>
